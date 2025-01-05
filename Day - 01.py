5354fc61627f8c217811778395f553c75ff539c1
Mohit-990 <upadhyaymohit990@gmail.com>
2025-01-05 21:56:59 +0530

    Add files via upload

---
+++ Day - 01.py
@@ -0,0 +1,7 @@
+# Python Band Name Generator
+print("Welcome to yhe Band Name Generator")
+# Ask for the name of city the user grew up in
+city = input("What is the name of the city you grew up in ?\n")
+# Ask for the name of pet
+pet_name = input("What is the name of your pet ?\n")
+print(city +" "+ pet_name)
